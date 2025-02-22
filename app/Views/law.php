e1539d786742fa6333fc20089db30b423533a889
Rum <rumsnownsk@gmail.com>
2025-02-22 17:20:37 +0700

    created and filled in page a low

--- app/Views/law.php
+++ app/Views/law.php
@@ -1,5 +1,55 @@
 <div class="container">
     <div class="mainContent">
-        page law
+        <h2>решение совета депутатов города новосибирска</h2>
+        <div class="lawItem">
+            <div class="lawItem_whatIs">
+                <p class="law_num">№469</p>
+                <p class="law_date">от 27 сентября 2017 г.</p>
+            </div>
+            <div class="lawItem_desc">
+                <p>о правилах благоустройства территории города новосибирска и признании утратившими силу отдельных
+                    решений совета депутатов города новосибирска</p>
+            </div>
+        </div>
+        <h2 style="margin-top: 40px;">постановления мэрии города новосибирска</h2>
+        <div class="lawItem">
+            <div class="lawItem_whatIs">
+                <p class="law_num">№678</p>
+                <p class="law_date">от 3 марта 2021 г.</p>
+            </div>
+            <div class="lawItem_desc">
+                <p>о порядке формирования паспорта фасадов здания, сооружения, нестационнарного объекта на территории
+                    города новосибирска</p>
+            </div>
+        </div>
+        <div class="lawItem">
+            <div class="lawItem_whatIs">
+                <p class="law_num">№3545</p>
+                <p class="law_date">от 8 октября 2021 г.</p>
+            </div>
+            <div class="lawItem_desc">
+                <p>о положении о формах указателей с наименованиями улиц и номерами домов, размещаемых на зданиях,
+                    сооружениях в городе новосибирске, и требованиях к таким указателям</p>
+            </div>
+        </div>
+        <div class="lawItem">
+            <div class="lawItem_whatIs">
+                <p class="law_num">№3545</p>
+                <p class="law_date">от 29 октября 2019 г.</p>
+            </div>
+            <div class="lawItem_desc">
+                <p>об архитектурно-художественом регламенте размещения информационных и рекламных конструкций в городе
+                    новосибирске</p>
+            </div>
+        </div>
+        <div class="lawItem">
+            <div class="lawItem_whatIs">
+                <p class="law_num">№7437</p>
+                <p class="law_date">от 27 декабря 2023 г.</p>
+            </div>
+            <div class="lawItem_desc">
+                <p>о категориях городской территории</p>
+            </div>
+        </div>
     </div>
 </div>
