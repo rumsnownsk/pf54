792345d9995aebba5626ff86f38be79d39b144e8
Rum <rumsnownsk@gmail.com>
2025-03-01 16:20:35 +0700

    добавили страницу Контакты

---
+++ app/Views/contacts.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="mainContent">
+        <h2>Проектно-экспертная организация ООО «Паспорт фасадов»</h2>
+        <p class="font_classic2">Красный проспект 14 офис 703</p>
+        <p class="font_classic2">Время работы 10:00-17:00</p>
+        <p class="font_classic2">телефон: 223-77-49, сот. 8-913-944-88-50</p>
+        <p class="font_classic2">e-mail: pfnsk@list.ru</p>
+    </div>
+</div>
