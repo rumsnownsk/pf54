8db66c10b4459af0032416f9bedc336fd0858dc0
Rum <rumsnownsk@gmail.com>
2025-03-03 18:56:05 +0700

    fix page Contacts and first works start

--- app/Views/contacts.php
+++ app/Views/contacts.php
@@ -1,9 +1,16 @@
 <div class="container">
     <div class="mainContent">
         <h2>Проектно-экспертная организация ООО «Паспорт фасадов»</h2>
-        <p class="font_classic2">Красный проспект 14 офис 703</p>
-        <p class="font_classic2">Время работы 10:00-17:00</p>
-        <p class="font_classic2">телефон: 223-77-49, сот. 8-913-944-88-50</p>
-        <p class="font_classic2">e-mail: pfnsk@list.ru</p>
+        <div class="address">
+            <div class="address_desc">
+                <p class="font_classic"><b>Адрес:</b> г.Новосибирск,<br>
+                Красный проспект 14, офис 703<br>
+                (Административный центр "Сибревком")</p>
+                <p class="font_classic"><b>Телефон:</b> 223-77-49, сот. 8-913-944-88-50</p>
+                <p class="font_classic"><b>e-mail:</b> pfnsk@list.ru</p>
+                <p class="font_classic"><b>Время работы:</b> 10:00-17:00</p>
+            </div>
+                <img class="photo_sibrevcom" src="/images/common/sibrevcom.jpeg" alt='адрес ООО "Паспорт Фасадов"'>
+        </div>
     </div>
 </div>
