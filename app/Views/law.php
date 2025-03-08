c9e5d4a64d241404770b60ab643d516277eb6a93
Rum <rumsnownsk@gmail.com>
2025-03-08 14:16:03 +0700

    continued worked with law.php

--- app/Views/law.php
+++ app/Views/law.php
@@ -3,12 +3,17 @@
         <h2>решение совета депутатов города новосибирска</h2>
         <div class="lawItem">
             <div class="lawItem_whatIs">
-                <a href="https://docs.yandex.ru/docs/view?tm=1740386193&tld=ru&lang=ru&name=Pravila-blagoustroystva-goroda-Novosibirska-ot-27-sentyabrya-2017-g.-N-469-pr2019.pdf&text=№469%20от%2027%20сентября%202017%20г.&url=https%3A%2F%2Fnovo-sibirsk.ru%2Fupload%2Fiblock%2F2d7%2FPravila-blagoustroystva-goroda-Novosibirska-ot-27-sentyabrya-2017-g.-N-469-pr2019.pdf&lr=65&mime=pdf&l10n=ru&sign=61c419d329b9c08c9bb0b6f881ae9d30&keyno=0&nosw=1&serpParams=tm%3D1740386193%26tld%3Dru%26lang%3Dru%26name%3DPravila-blagoustroystva-goroda-Novosibirska-ot-27-sentyabrya-2017-g.-N-469-pr2019.pdf%26text%3D%25E2%2584%2596469%2B%25D0%25BE%25D1%2582%2B27%2B%25D1%2581%25D0%25B5%25D0%25BD%25D1%2582%25D1%258F%25D0%25B1%25D1%2580%25D1%258F%2B2017%2B%25D0%25B3.%26url%3Dhttps%253A%2F%2Fnovo-sibirsk.ru%2Fupload%2Fiblock%2F2d7%2FPravila-blagoustroystva-goroda-Novosibirska-ot-27-sentyabrya-2017-g.-N-469-pr2019.pdf%26lr%3D65%26mime%3Dpdf%26l10n%3Dru%26sign%3D61c419d329b9c08c9bb0b6f881ae9d30%26keyno%3D0%26nosw%3D1" class="law_num" target="_blank">№469</a>
+                <p class="law_num" >№469</p>
                 <p class="law_date">от 27 сентября 2017 г.</p>
+
             </div>
             <div class="lawItem_desc">
                 <p>о правилах благоустройства территории города новосибирска и признании утратившими силу отдельных
                     решений совета депутатов города новосибирска</p>
+                <div class="law_a">
+                    <a href="#" target="_blank">читать</a>
+                    <a href="#" target="_blank">скачать</a>
+                </div>
             </div>
         </div>
         <h2 style="margin-top: 40px;">постановления мэрии города новосибирска</h2>
@@ -20,6 +25,10 @@
             <div class="lawItem_desc">
                 <p>о порядке формирования паспорта фасадов здания, сооружения, нестационнарного объекта на территории
                     города новосибирска</p>
+                <div class="law_a">
+                    <a href="#" target="_blank">читать</a>
+                    <a href="#" target="_blank">скачать</a>
+                </div>
             </div>
         </div>
         <div class="lawItem">
@@ -30,6 +39,10 @@
             <div class="lawItem_desc">
                 <p>о положении о формах указателей с наименованиями улиц и номерами домов, размещаемых на зданиях,
                     сооружениях в городе новосибирске, и требованиях к таким указателям</p>
+                <div class="law_a">
+                    <a href="#" target="_blank">читать</a>
+                    <a href="#" target="_blank">скачать</a>
+                </div>
             </div>
         </div>
         <div class="lawItem">
@@ -40,6 +53,10 @@
             <div class="lawItem_desc">
                 <p>об архитектурно-художественном регламенте размещения информационных и рекламных конструкций в городе
                     новосибирске</p>
+                <div class="law_a">
+                    <a href="#" target="_blank">читать</a>
+                    <a href="#" target="_blank">скачать</a>
+                </div>
             </div>
         </div>
         <div class="lawItem">
@@ -49,6 +66,10 @@
             </div>
             <div class="lawItem_desc">
                 <p>о категориях городской территории</p>
+                <div class="law_a">
+                    <a href="#" target="_blank">читать</a>
+                    <a href="#" target="_blank">скачать</a>
+                </div>
             </div>
         </div>
     </div>
